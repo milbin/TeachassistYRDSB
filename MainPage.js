124f756ed3b54986c63812988042c72e15f60e92
Sarah Tran <sarahtran0814@gmail.com>
2020-04-08 22:54:04 -0400

    added async storage and header (not working)

---
+++ MainPage.js
@@ -0,0 +1,6 @@
+import 'react-native-gesture-handler';
+import React, {Component} from 'react';
+import {View, Text, Image, TextInput, TouchableOpacity} from 'react-native';
+import {Icon} from 'react-native-elements';
+import {styles, Theme, elements} from './Styles';
+import talogo from './assets/images/talogo.png';
